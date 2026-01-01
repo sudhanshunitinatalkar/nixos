2fab82cd55ff1cb759edc57d6ec85ac4581dcf68
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-01-01 22:43:03 +0530

    update

--- host/configuration.nix
+++ host/configuration.nix
@@ -59,13 +59,16 @@
     };
     
     postgresql = 
-    {
-      enable = true;
-      authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
-      '';
-    };
+        {
+          enable = true;
+          authentication = pkgs.lib.mkOverride 10 ''
+            #type database  DBuser  auth-method
+            local all       all     trust
+            # # Allow ThingsBoard (and other apps) to connect via TCP/IP
+            # host  all       all     127.0.0.1/32   scram-sha-256
+            # host  all       all     ::1/128        scram-sha-256
+          '';
+        };
 
 
     # cloudflared = {
