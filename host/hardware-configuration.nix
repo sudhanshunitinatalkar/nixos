d43d11f6f24a24309d238224205ce81e22583a2b
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-01-16 00:55:38 +0530

    update

--- host/hardware-configuration.nix
+++ host/hardware-configuration.nix
@@ -46,7 +46,7 @@
     };
  
   swapDevices = [
-    swapDevices = [ { device = "/swap/swapfile"; } ];
+    { device = "/swap/swapfile"; } ];
   ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
