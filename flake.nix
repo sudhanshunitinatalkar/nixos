828a65d7d57e5cb7423c9a76ae2fc0292495d2b2
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-02-28 00:08:33 +0530

    update

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = 
   {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = 
     {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     zen-browser = {
