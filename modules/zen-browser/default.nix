5608cf236e7668378ec80e5ace8321ad7b1c20b1
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-03-20 11:41:27 +0530

    update

--- modules/zen-browser/default.nix
+++ modules/zen-browser/default.nix
@@ -4,6 +4,10 @@
       home.packages = [ 
         inputs.zen-browser.packages."${pkgs.stdenv.hostPlatform.system}".default 
       ];
+
+      # This manually links the Plasma bridge into Zen's config folder
+      home.file.".mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json".source = 
+        "${pkgs.kdePackages.plasma-browser-integration}/lib/mozilla/native-messaging-hosts/org.kde.plasma.browser_integration.json";
     };
   };
 }
