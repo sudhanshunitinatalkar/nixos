e6d7b5e5612a769158daf670251f407c16225bb0
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-03-20 10:51:07 +0530

    update

--- modules/cosmic/default.nix
+++ modules/cosmic/default.nix
@@ -8,18 +8,9 @@
             system76-scheduler.enable = true;
         };
 
-        environment.sessionVariables = {
-            # Bypasses strict Wayland security so your clipboard works globally
-            COSMIC_DATA_CONTROL_ENABLED = "1";
-            
-            # Forces Electron apps (VSCode, etc.) to use Wayland natively, eliminating XWayland stutter
+        environment.sessionVariables = {            
             NIXOS_OZONE_WL = "1";
         };
 
-        environment.systemPackages = with pkgs; [
-            # Essential Wayland utilities
-            wl-clipboard
-            wayland-utils
-        ];
     };
 }
